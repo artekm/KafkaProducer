a8f5cddb7f0ecf6983c7d269c73208cb90874984
artekm <44265668+artekm@users.noreply.github.com>
2019-10-25 16:24:51 +0200

    Initial commit

---
+++ src/main/java/artur/KafkaProducerAM.java
@@ -0,0 +1,39 @@
+package artur;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class KafkaProducerAM {
+    private static final String DICTIONARY = "english10000.txt";
+    private static final String PROPERTIES = "producer.properties";
+    private static final String TOPIC = "samsung";
+
+    public static void main(String[] args) throws Exception {
+        List<String> words = Files.readAllLines(Paths.get(DICTIONARY));
+
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(PROPERTIES)) {
+            properties.load(input);
+        }
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        System.out.println("Press ENTER to start");
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+
+        for (String word : words) {
+            System.out.println("Sending " + word);
+            producer.send(new ProducerRecord<>(TOPIC, word));
+            Thread.sleep(500);
+        }
+    }
+}
